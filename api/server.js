c32d30500097fbd82c858f2f94b34f6fef88a7e4
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-11 18:09:54 -0400

    delete request in place

--- api/server.js
+++ api/server.js
@@ -55,4 +55,19 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  Model.remove(id)
+    .then((removedUser) => {
+      if (!removedUser) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" });
+      } else {
+        res.status(200).json(removedUser);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "The user could not be removed" });
+    });
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
